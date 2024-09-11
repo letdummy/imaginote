c702716cc29f66bd582293f2c9c271f938924096
letdummy <71609913+letdummy@users.noreply.github.com>
2024-09-12 05:46:06 +0700

    chore ♻️: add initial project files and structure

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client';
+
+// import style
+import './styles/index.css';
+
+const root = createRoot(document.getElementById('root'));
+root.render(<h1>Hello, World!</h1>);
